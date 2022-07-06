15c9a72a51ec11022abe0353734d7e93f868c91b
Angela Dimon <angeladimon91@gmail.com>
2022-07-06 16:26:38 +0300

    receive post to endpoint and call yahoo finance api to get historical stock prices

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/alpacahq/alpaca-trade-api-go/v2/alpaca"
@@ -52,16 +55,79 @@ func (h *handler) newClient() alpaca.Client {
 	return client
 }
 
+func (h *handler) calculateRSI(ticker string, interval string, length int32) {
+	log.Printf("Calculating RSI with length %d, ticker %s, and interval %s", length, ticker, interval)
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodGet, "https://yfapi.net/v8/finance/spark?", nil)
+	if err != nil {
+		log.Fatalf("Error creating http request %v\n", err)
+	}
+
+	// appending to existing query args
+	q := req.URL.Query()
+	q.Add("interval", interval)
+	q.Add("range", "1d")
+	q.Add("symbols", ticker)
+
+	// assign encoded query string to http request
+	req.URL.RawQuery = q.Encode()
+
+	key, ok := os.LookupEnv("YAHOO_API_KEY")
+	if !ok {
+		log.Fatal("can not read env variable")
+	}
+
+	// add headers
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("X-API-KEY", key)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Error sending request to yahoo finance %v\n", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatalf("Error reading body %v\n", err)
+	}
+
+	log.Println(string(body))
+}
+
 func (h *handler) trade(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Println("Executing trade()")
+
 	// retrieve alpaca account information
 	acct, err := h.client.GetAccount()
 	if err != nil {
 		log.Fatalf("Failed to get the account %v", err)
 	}
 
-	fmt.Printf("%+v\n", *acct)
-	body := fmt.Sprintf("Request successful with api gateway response. Account info: %+v\n", *acct)
+	if req.HTTPMethod == "GET" {
+		body := fmt.Sprintf("Request successful with api gateway response. Account info: %+v\n", *acct)
+		response := events.APIGatewayProxyResponse{Body: body, StatusCode: 200}
+		return response, nil
+	}
+
+	type Trade struct {
+		Ticker   string `json:"ticker"`
+		Interval string `json:"interval"`
+	}
+
+	b := []byte(req.Body)
+	var resp Trade
+	err = json.Unmarshal(b, &resp)
+
+	if err != nil {
+		log.Fatalf("Error unmarshalling json %v\n", err)
+	}
+
+	h.calculateRSI(resp.Ticker, resp.Interval, 14)
+
+	body := fmt.Sprintf("Request body was: %+v\n", req.Body)
 	response := events.APIGatewayProxyResponse{Body: body, StatusCode: 200}
 	return response, nil
 }
